50907f6be39aec5e532b77c070bfc658d876348a
Minifyre <minifyre@gmail.com>
2018-12-03 15:34:27 -0600

    +output.tile
    This will generate tile elements.

--- src/output.js
+++ src/output.js
@@ -1,5 +1,17 @@
 output.render=function(state)
 {
 
-}//@todo upstream to v
+
+	return [v('main.grid',{},...tiles)]
+}
+output.tile=function({file},txt,{x,y})
+{
+	const
+	{height:h,width:w}=file.data,
+	[left,top]=[x/w,y/h].map(x=>util.dec2percent(x,0)+'%'),
+	style=output.style({left,top})
+
+	return v('.tile',{style},txt)
+}
+//@todo upstream to v
 output.style=props=>Object.entries(props).map(([key,val])=>key+':'+val+';').join(' ')
