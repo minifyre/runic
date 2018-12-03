88c4433607d9e16e5828532cbfad0ef72a6f7bcb
Minifyre <minifyre@gmail.com>
2018-12-03 15:06:34 -0600

    +logic.normalize
    Normalize will setup a fresh state, based on config.

--- src/logic.js
+++ src/logic.js
@@ -1,1 +1,16 @@
+logic.normalize=function(opts)
+{
+	const
+	state=util.mkState(opts),
+	{height:h,width:w}=state.file.data,
+	l=w*h,
+	grid=Array(l).fill(0)
+	//populate grid
+	Array(Math.floor(l/2)).fill(1).forEach(()=>grid[logic.rand(l)]+=1)
+
+	state.file.data.grid=grid
+
+	return state
+}
+
 logic.rand=(num,seed=Math.random())=>Math.floor(seed*num)
