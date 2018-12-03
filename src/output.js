034d42361ee9595b1b1d27600579fc6d27ed9bb7
Minifyre <minifyre@gmail.com>
2018-12-03 15:40:20 -0600

    +tile dimensions

--- src/output.js
+++ src/output.js
@@ -21,7 +21,8 @@ output.tile=function({file},txt,{x,y})
 	const
 	{height:h,width:w}=file.data,
 	[left,top]=[x/w,y/h].map(x=>util.dec2percent(x,0)+'%'),
-	style=output.style({left,top})
+	[height,width]=[w,h].map(x=>(100/x)+'%'),
+	style=output.style({height,left,top,width})
 
 	return v('.tile',{style},txt)
 }
