83ad0c9f163493049a945e9c2571766d4818c691
Minifyre <minifyre@gmail.com>
2018-12-04 14:26:29 -0600

    reworked args for consistency

--- matrix.js
+++ matrix.js
@@ -1,5 +1,5 @@
-const matrix=(w=0,h=w,fill)=>({array:Array(h*w).fill(fill),height:h,width:w})
-matrix.at=(w,h,a,x,y)=>a[w*y+x]
+const matrix=(w=0,h=w,fill=1)=>Array(h*w).fill(fill)
+matrix.at=(w,h,a,{x,y})=>a[w*y+x]
 matrix.col=(w,h,a,x)=>Array(h).fill(1).map((_,i)=>a[i*w+x])
 matrix.i2col=(w,h,a,i)=>matrix.col(a,matrix.i2x(a,i))
 matrix.i2pt=(w,h,a,i)=>({x:matrix.i2x(w,h,a,i),y:matrix.i2y(w,h,a,i)})
