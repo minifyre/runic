6e4cad1695f00c224bf7ba5177ee48a13d8972c8
Minifyre <minifyre@gmail.com>
2018-12-03 18:11:21 -0600

    +adj to cache
    This property will be used to determine how far to translate move-able tiles.

--- src/util.js
+++ src/util.js
@@ -2,14 +2,16 @@ util.dec2percent=(float,precision=4)=>(float*100).toFixed(precision)
 util.mkRendererCache=function(state)
 {
 	const
+	{adj}=state.view,
 	{grid,height:h,width:w}=state.file.data,
 	i2pt=i=>matrix.i2pt(w,h,grid,i),
 	tileset=util.tilesets[state.view.tileset],
 	max=Math.max(...grid)
 
-	return {grid,h,w ,max, i2pt,tileset}//shortcuts, derrived, fns
+	return {adj,grid,h,w ,max, i2pt,tileset}//shortcuts, derrived, fns
 }
 util.tilesets=
 {
 	'2s':exp=>Math.pow(2,exp)
 }
+util.evt2pt=({pageX:x,pageY:y})=>({x,y})
