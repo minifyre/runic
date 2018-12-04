c97695a255ef87729b01554d344ce93a83457b17
Minifyre <minifyre@gmail.com>
2018-12-03 18:14:04 -0600

    +tile translation on user movement
    This shifts the tiles in one direction based on user input.

--- src/output.js
+++ src/output.js
@@ -2,19 +2,23 @@ output.render=function(state)
 {
 	const
 	cache=util.mkRendererCache(state),
-	tiles=cache.grid.map(curry(output.tile,cache)).filter(x=>!!x)
+	tiles=cache.grid.map(curry(output.tile,cache)).filter(x=>!!x),
+	
+	data={pointerdown:'slide'},
+	pointerdown=evt=>input(state,evt)
 
-	return [v('main.grid',{},...tiles)]
+	return [v('main.grid',{data,on:{pointerdown}},...tiles)]
 }
-output.tile=function({h,w, max, i2pt,tileset},val,i)
+output.tile=function({adj,h,w, max, i2pt,tileset},val,i)
 {
 	if(!val) return
 
 	const
 	{x,y}=i2pt(i),
-	[left,top]=[x/w,y/h].map(x=>util.dec2percent(x,0)+'%'),
+	[left,top]=[x/w,y/h]
+		.map(x=>util.dec2percent(x,0)+'%'),
 	[height,width]=[w,h].map(x=>(100/x)+'%'),
-	style=output.style({height,left,top,width}),
+	style=output.style({height,left:`calc(${left} + ${adj.x}px)`,top:`calc(${top} + ${adj.y}px)`,width}),
 	attrs={style}
 
 	if(val===max) attrs.class='max'
