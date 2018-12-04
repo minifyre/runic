a5f8a96386a3d66c039a0e073e58a88ef7800285
Minifyre <minifyre@gmail.com>
2018-12-03 18:12:07 -0600

    +input.slide
    This handles user input to move tiles.

---
+++ src/input.js
@@ -0,0 +1,47 @@
+input.slide=function(state,evt)
+{	
+	if(state.view.sliding) return
+
+	logic.toggleInput(state)
+
+	const
+	target=evt.path.find(el=>el.matches('.grid')),
+	{pointerId:pointer}=evt,
+	pt0=util.evt2pt(evt),
+	move=function(evt)
+	{
+		if(evt.pointerId!==pointer) return
+
+		const
+		{adj}=state.view,
+		pt=util.evt2pt(evt),
+		h=pt.x-pt0.x,
+		v=pt.y-pt0.y
+
+		//@todo dry!
+		if(!adj.x&&!adj.y)
+		{
+			if(Math.abs(h)>Math.abs(v)) adj.x=h
+			else adj.y=v
+		}
+		else if(adj.x&&(Math.abs(h)>Math.abs(v))) adj.x=h
+		else if(adj.y) adj.y=v
+	},
+	stop=function(evt)
+	{
+		if(evt.pointerId!==pointer) return
+
+		target.removeEventListener('pointermove',move)
+		target.removeEventListener('pointerup',stop)
+
+
+		//@todo calculate move here
+
+		state.view.adj.x=0
+		state.view.adj.y=0
+		logic.toggleInput(state)
+	}
+
+	target.addEventListener('pointermove',move)
+	target.addEventListener('pointerup',stop)
+}
